8545c0344eb72e4cddce57995042076432cf471e
Benjamin1976 <bp_coding@yahoo.com.au>
2025-04-07 20:28:10 +1000

    fixing connectivity

--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -13,7 +13,7 @@ import jwt from "jsonwebtoken";
 import { compare } from "bcryptjs";
 import { getJwt, log } from "../utils";
 
-const DBL = 1;
+const DBL = 2;
 const dp = "routes.auth";
 
 export const authRouter = express.Router();
